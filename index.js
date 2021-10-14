0ab5bc720bbbcaecf0a542c7401eaebde4310e2f
k2u6m5i3r <wunder49@yandex.by>
2021-10-14 22:56:34 +0300

    noFunction

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello World!!!");
+//решение без чистой функции
+let str, ans;
+ans = 0; //переменная подсчёта гластных
+
+do {
+    str = prompt("введите строку на русском языке!");
+} while (  str.length == 0 );
+//str = "окещлещрлещлЗЩлазлвзщылавзщыла взщыалвщыаыЛВЩЗФЫВЛ ФЫВЛФЫЩв";
+for (let index = 0; index < str.length; index++) {
+    //в нижний регистр чтобы не сравнивать большие буквы, а только маленькие строчный
+    const element = str[index].toLowerCase();
+    if (element == 'а' || element == 'о' || element == 'и' ||
+        element == 'е' || element == 'ё' || element == 'э' ||
+        element == 'ы' || element == 'у' || element == 'ю' || element == 'я'){
+            ans++;
+        }
+}
+console.log(ans);
