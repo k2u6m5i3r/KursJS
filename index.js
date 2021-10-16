a068f084326725db29a6668282d4fd642e6beb80
k2u6m5i3r <wunder49@yandex.by>
2021-10-16 20:12:59 +0300

    РешениеЗадачмСлайдов

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World!!!");
+let numberUser = prompt('введите число!');
+//alert(`вы ввели ${numberUser}`);
+if (numberUser == 'NaN') {
+    //если ввод NaN пишем что введен NaN
+    numberUser = prompt(`вы ввели NaN ${numberUser}. ВВедите число`);//вводим заново число строку уже правильно
+} 
+if ( String(+numberUser)  == 'NaN' || numberUser == '') {//пользователь ввел некоректно число или пустую строку
+    numberUser = prompt(`вы ввели ${numberUser}. ВВедите число`);//вводим заново  число строку уже правильно
+}
+//второй раз введено точно число строкой
+if (numberUser > 0 ) {//
+    alert(`вы ввели число ${numberUser}. Число положительное`);
+} else {
+    alert(`вы ввели число ${numberUser}. Число отрицательное`);
+}
+
+console.log(typeof numberUser);//работа со строкой
