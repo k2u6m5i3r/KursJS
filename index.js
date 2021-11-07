bf626c54130adb781f2f54122a73f56b7fa82900
k2u6m5i3r <wunder49@yandex.by>
2021-11-07 13:48:33 +0300

    function

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-//решение без чистой функции
-let str, ans;
-ans = 0; //переменная подсчёта гластных
-
+//решение с "чистой функцией"
+let str;
 do {
     str = prompt("введите строку на русском языке!");
     str = String(str);// если пользователь нажал Отмена то возращается null приведение превращает в строку и проверка на 'null'
 } while (  str.length == 0  || str == 'null');
 //str = "окещлещрлещлЗЩлазлвзщылавзщыла взщыалвщыаыЛВЩЗФЫВЛ ФЫВЛФЫЩв";
-for (let index = 0; index < str.length; index++) {
-    //в нижний регистр чтобы не сравнивать большие буквы, а только маленькие строчный
-    const element = str[index].toLowerCase();
-    if (element == 'а' || element == 'о' || element == 'и' ||
-        element == 'е' || element == 'ё' || element == 'э' ||
-        element == 'ы' || element == 'у' || element == 'ю' || element == 'я'){
-            ans++;
-        }
+
+function getNumberLiter(str) {
+    let ans = 0; //переменная подсчёта гластных
+    for (let index = 0; index < str.length; index++) {
+        //в нижний регистр чтобы не сравнивать большие буквы, а только маленькие строчный
+        const element = str[index].toLowerCase();
+        if (element == 'а' || element == 'о' || element == 'и' ||
+            element == 'е' || element == 'ё' || element == 'э' ||
+            element == 'ы' || element == 'у' || element == 'ю' || element == 'я'){
+                ans++;
+            }
+    }
+    return ans;        
 }
-console.log(ans);
+console.log(getNumberLiter(str));
