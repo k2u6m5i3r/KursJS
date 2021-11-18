8396e1fcf8f6898fa600c94746ef25c630651cd6
k2u6m5i3r <wunder49@yandex.by>
2021-11-18 21:42:24 +0300

    Решение

--- index.js
+++ index.js
@@ -23,14 +23,36 @@ function getNumberForEach(str) {
 console.log(getNumberForEach(str));
 
 function getNumberfilter(str) {
-    let ans = 0; //переменная подсчёта гластных
-
-    return ans;  
+    let ans = []; //фильтр возращает массив элементов прошедшие проверку.
+    ans = str.split(''); // разбил строку на элементы массива
+    ans = ans.filter(element => {
+        element = element.toLowerCase();
+        if (element == 'а' || element == 'о' || element == 'и' ||
+        element == 'е' || element == 'ё' || element == 'э' ||
+        element == 'ы' || element == 'у' || element == 'ю' || element == 'я'){  
+            return element;
+        } 
+    }) 
+    return ans.length; // количество элементов прошедшие проверку  
 }
-console.log(getNumberForEach(str));
+console.log(getNumberfilter(str));
 
 function getNumberReduce(str) {
     let ans = 0; //переменная подсчёта гластных
-
+    let arrayTemp = []; // временный массив
+    arrayTemp = str.split(''); // разбил строку на элементы массива
+    ans = arrayTemp.reduce(function (acc, element) {
+        element = element.toLowerCase();
+        if(element == 'а' || element == 'о' || element == 'и' ||
+        element == 'е' || element == 'ё' || element == 'э' ||
+        element == 'ы' || element == 'у' || element == 'ю' || element == 'я'){
+            // console.log(element);
+            //acc.push(element);
+            acc++;
+        }
+        return acc;
+    }, 0);
+    // console.log(ans);
     return ans;  
 }
+console.log(getNumberReduce(str));
