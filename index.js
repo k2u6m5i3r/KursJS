a4f188143152c05b32c1b30481dc6eea9c6d64e1
k2u6m5i3r <wunder49@yandex.by>
2021-12-13 22:01:47 +0300

    HomeTask class

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello World!!!");
+// console.log("Hello World!!!");
+class HashStorageClass{
+    constructor(){
+        this.storageObject = {}; //объект пудличный для храниния, в нем будут создаваться поля - значения(ключ:значение)
+    }
+    addValue(){
+
+    }
+    getValue(){
+
+    }
+    deleteValue(){
+
+    }
+    getKeys(){
+        
+    }
+}
+let drinkStorage = new HashStorageClass();
+console.log(drinkStorage);
