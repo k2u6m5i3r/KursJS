35a77615749747388a5436c55bc1382d61faabb4
k2u6m5i3r <wunder49@yandex.by>
2021-12-24 05:46:42 +0300

    fetch

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!!!");
+// console.log("Hello World!!!");
+// XMLHttpRequest
+// let myRequests = new XMLHttpRequest();
+
+//
+
+// Fetch парсим посты
+
+// let myFetch;
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(json => {
+      for(let i in json)
+      console.log(json[i].title);
+  });
+
+
+
+
