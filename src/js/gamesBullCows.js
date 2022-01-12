ce0813fdeb7ac613c3b34883230056f17773a88c
k2u6m5i3r <wunder49@yandex.by>
2022-01-12 15:52:42 +0300

    errorGames-Fix

--- src/js/gamesBullCows.js
+++ src/js/gamesBullCows.js
@@ -47,10 +47,17 @@ function checkingNumberOfBulls(numberFromUser, numberFromComputer) { // пров
 // console.log(`количество коров`,checkingNumberOfCows(userNumber, computerNumber));
 function checkingNumberUser(numberFromUser){
     let ans = false;
-    if(numberFromUser[0] != numberFromUser[1] && numberFromUser[0] != numberFromUser[2] && numberFromUser[0] != numberFromUser[3] ){
-        ans = true;
-    } else {
-        ans = false;
+
+    // Проверить
+    for(let i = 0; i < 4; i++){
+        for(let j = i; j < 4; j++ ){
+            if(i != j)
+            if( numberFromUser[i] == numberFromUser[j]  ){
+                return false;
+            } else{
+               ans = true;
+            }
+        }
     }
     return ans;
 }
